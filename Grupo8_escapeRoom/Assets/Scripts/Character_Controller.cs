5088a55c10ec00237d03f213b0c0873d4448c0b3
anacr2002 <72752737+anacr2002@users.noreply.github.com>
2022-04-28 13:10:40 +0100

    Finished_CharacterController

--- Grupo8_escapeRoom/Assets/Scripts/Character_Controller.cs
+++ Grupo8_escapeRoom/Assets/Scripts/Character_Controller.cs
@@ -17,23 +17,35 @@ public class Character_Controller : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey("w") == true)
-        {
-            transform.localPosition += transform.forward * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey("a") == true)
-        {
-            transform.localPosition += transform.right * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey("s") == true)
-        {
-            transform.localPosition += transform.forward * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey("d") == true)
-        {
-            transform.localPosition += transform.right * Time.deltaTime * moveSpeed;
-        }
 
 
-    }
-}
+        //if (Input.GetKey("w") == true)
+        //{
+        //    transform.localPosition += transform.forward * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey("a") == true)
+        //{
+        //    transform.localPosition += transform.right * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey("s") == true)
+        //{
+        //    transform.localPosition += transform.forward * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey("d") == true)
+        //{
+        //    transform.localPosition += transform.right * Time.deltaTime * moveSpeed;
+        //}
+
+        
+                float valueH = Input.GetAxis("Horizontal");
+                transform.position += transform.right * valueH * Time.deltaTime * moveSpeed;
+
+                float valueV = Input.GetAxis("Vertical");
+                transform.position += transform.forward * valueV * Time.deltaTime * moveSpeed;
+
+                float angle = 0.1f;
+                float horizontal = Input.GetAxis("Horizontal");
+                transform.Rotate(transform.up, horizontal * angle, Space.Self);
+            }
+        }
+
