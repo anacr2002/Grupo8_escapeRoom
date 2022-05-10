569801e4fc53078bb89264b604e81dd195f97f91
anacr2002 <72752737+anacr2002@users.noreply.github.com>
2022-05-10 11:22:41 +0100

    input_system + movement

---
+++ Grupo8_escapeRoom/Assets/Scripts/Player_Movemment.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player_Movemment : MonoBehaviour
+{
+    private CharacterController characterController;
+    private float speed = 0.5f;
+    private Vector2 playerMovementInput;
+   
+    void Start()
+    {
+       
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        Vector3 movement = new Vector3(playerMovementInput.x, 0.0f, playerMovementInput.y);
+        characterController.SimpleMove(movement * speed);
+    }
+
+    void OnMove (InputValue iv)
+    {
+        Debug.Log("Player a dar caralho");
+        playerMovementInput = iv.Get<Vector2>();
+    }
+}
